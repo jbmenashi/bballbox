7e241ecf4f1d91c809d56718498d775e55356948
jbmenashi <jbmenashi@gmail.com>
2018-11-29 11:28:35 -0500

    add destroy buttons for each class

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   has_many :boxscores
   has_many :players, through: :boxscores
+  validates :date, presence: true
+  validates :opponent, presence: true, length: { is: 3 }
 
   def readable_datetime
     "#{self.date.strftime('%B %d, %Y')}"
